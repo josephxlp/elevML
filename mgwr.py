ee4d8611d21118c31430d90dd0911d1a049b803e
josephxlp <l.joseph.p@hotmail.com>
2025-07-01 17:16:20 +0100

    Gdem # removed dprep_blocks.ipynb
    * xEnsembles.py skip OPTe.py
    
    * mgwr.py added
    
    * fmin_get checking added
    
    * removed dprep_blocks.ipynb
    
    ---------
    
    Co-authored-by: jzphlp <josephluntadilap@gmail.com>

--- mgwr.py
+++ mgwr.py
@@ -6,7 +6,7 @@ from xSagaDownscale import gwrdownxcale
 from xEnsembles import ensemble_prediction
 
 def pipeline_dxcale(xpath, ypath, geoid_fn, wdir,name = 'gwrDTM_'):
-    ti = time.perf_counter()
+    
     start_time = datetime.now()
     """
     Pipeline for downscaling GDEM data using GWR and ensemble methods.
@@ -18,24 +18,29 @@ def pipeline_dxcale(xpath, ypath, geoid_fn, wdir,name = 'gwrDTM_'):
     dw_weighting_list = [0,1,2,3]
     outdir = f"{wdir}/{expname}" #wdir >out_dpath
     os.makedirs(outdir, exist_ok=True)
-
-    #gdemH_fn_tls = ypath.replace(".tif", "_EGM.tif")
-    #if not os.path.exists(gdemH_fn_tls):bcor_sub(fine_path=ypath, coarse_path=geoid_fn, output_path=gdemH_fn_tls) # add this into the dataprep 
-
     opath =  os.path.join(outdir,f'{expname}_{name}.tif')
-    overwrite= True#True  # only the 1st time :: fix this also 
-    aux_files = True 
-    clean = False
-    model_out = 1# d:0
+    overwrite=False
+    oaux=False
+    epsg_code=4979
+    clean=True
+    search_range=0
+    search_radius=10
+    dw_idw_power=2.0
+    dw_bandwidth=1.0
+    logistic=0
+    model_out=1
+    grid_system=None
+    fmin_run=True
+    
     gwrp_fn_list , fmin_fn_list = [],[]
     ti = time.perf_counter()
     for dw_weighting in dw_weighting_list:
-        gwrp_fn, fmin_fn = gwrdownxcale(xpath, ypath, opath, geoid_fn, 
-                    overwrite=False, oaux=False, epsg_code=4979, clean=True,
-                    search_range=0, search_radius=10, 
-                    dw_weighting=dw_weighting, dw_idw_power=2.0, dw_bandwidth=1.0,
-                    logistic=0, model_out=0, grid_system=None,
-                    fmin_run=True)
+        gwrp_fn, fmin_fn = gwrdownxcale(
+            xpath, ypath, opath, geoid_fn, 
+            overwrite=overwrite, oaux=oaux, epsg_code=epsg_code, clean=clean,
+            search_range=search_range, search_radius=search_radius, dw_weighting=dw_weighting, 
+            dw_idw_power=dw_idw_power, dw_bandwidth=dw_bandwidth, logistic=logistic, 
+            model_out=model_out, grid_system=grid_system,fmin_run=fmin_run)
         gwrp_fn_list.append(gwrp_fn)
         fmin_fn_list.append(fmin_fn)
 
@@ -45,9 +50,10 @@ def pipeline_dxcale(xpath, ypath, geoid_fn, wdir,name = 'gwrDTM_'):
 
     init_points, n_iter = 5,15#10,50 #5,15 #10,90 #10,50
 
-    rfile = xpath
+    #rfile = xpath
+    #pfiles = fmin_fn_list
     ename_fmin = f"{expname}_{name}_fmin"
-    pfiles = fmin_fn_list
+    
     print(f'Running {ename_fmin}')
     avg_raster, opt_raster = ensemble_prediction(ename=ename_fmin, pred_paths=fmin_fn_list, 
                                                 ref_path = xpath, 
@@ -56,7 +62,7 @@ def pipeline_dxcale(xpath, ypath, geoid_fn, wdir,name = 'gwrDTM_'):
 
 
     ename_gwrp = f"{expname}_{name}_gwrp"
-    pfiles = gwrp_fn_list
+    #pfiles = gwrp_fn_list
     print(f'Running {ename_gwrp}')
     avg_raster, opt_raster = ensemble_prediction(ename=ename_gwrp, pred_paths=gwrp_fn_list, 
                                                 ref_path = xpath, 
@@ -84,7 +90,7 @@ ypaths = [
 xpaths = [
     "/media/ljp238/12TBWolf/BRCHIEVE/GDEM/BLOCK/TLS/mosaik/TDEM_DEM_EGM_V_3_GAP_gF3_0_100_inv_dist.tif",
     "/media/ljp238/12TBWolf/BRCHIEVE/GDEM/BLOCK/TLS/mosaik/TDEM_DEM_EGM_V_3_GAP.tif",
-   # "/media/ljp238/12TBWolf/BRCHIEVE/GDEM/BLOCK/TLS/mosaik/TDEM_DEM_EGM.tif"
+    "/media/ljp238/12TBWolf/BRCHIEVE/GDEM/BLOCK/TLS/mosaik/TDEM_DEM_EGM.tif"
 ]
 
 for ypath in ypaths:
@@ -102,8 +108,7 @@ for ypath in ypaths:
         expname = pipeline_dxcale(xpath, ypath, geoid_fn=None, wdir=wdir,name = name) 
         fname = f"{name}_{expname}.tif"
         os.system(f'notify-send "{fname} finished" --expire-time=60000')
-        with open( 'progress.txt', 'a') as f:
-            f.write(f"{fname}\t{datetime.now()}\n")
+        
 
 
 
